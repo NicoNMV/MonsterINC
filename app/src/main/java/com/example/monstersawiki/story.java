799f49eee661c5718692566d47377eb1a7b2d3cb
Maria Cavalcante <maria.nascimento343@etec.sp.gov.br>
2022-09-17 20:33:27 -0300

    Tela de história e nome da aplicação

--- app/src/main/java/com/example/monstersawiki/story.java
+++ app/src/main/java/com/example/monstersawiki/story.java
@@ -2,7 +2,9 @@ package com.example.monstersawiki;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class story extends AppCompatActivity {
 
@@ -10,5 +12,32 @@ public class story extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+
+        getSupportActionBar().hide();
+    }
+
+    public void openID(View view){
+        Intent intent = new Intent(this, menuID.class);
+        startActivity(intent);
+    }
+    public void openCharacter(View view){
+        Intent intent = new Intent(this, character.class);
+        startActivity(intent);
+    }
+    public void openStory(View view){
+        Intent intent = new Intent(this, story.class);
+        startActivity(intent);
+    }
+    public void openDoor(View view){
+        Intent intent = new Intent(this, door.class);
+        startActivity(intent);
+    }
+    public void openUniversity(View view){
+        Intent intent = new Intent(this, university.class);
+        startActivity(intent);
+    }
+    public void openHome(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
