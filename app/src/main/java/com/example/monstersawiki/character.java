35c0ccae87e91324c88b123f80b71132aabff818
Maria Cavalcante <maria.nascimento343@etec.sp.gov.br>
2022-09-17 19:58:09 -0300

    Mudança de ícone

--- app/src/main/java/com/example/monstersawiki/character.java
+++ app/src/main/java/com/example/monstersawiki/character.java
@@ -2,7 +2,9 @@ package com.example.monstersawiki;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class character extends AppCompatActivity {
 
@@ -12,5 +14,9 @@ public class character extends AppCompatActivity {
         setContentView(R.layout.activity_character);
 
         getSupportActionBar().hide();
+    }
+    public void openID(View view){
+        Intent intent = new Intent(this, menuID.class);
+        startActivity(intent);
     }
 }
