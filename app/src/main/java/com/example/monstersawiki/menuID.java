fab91201846aa5cc8a410078cf5954e7ac885a81
Maria Cavalcante <maria.nascimento343@etec.sp.gov.br>
2022-09-17 01:35:04 -0300

    Finalização de códigos envolvendo ID

--- app/src/main/java/com/example/monstersawiki/menuID.java
+++ app/src/main/java/com/example/monstersawiki/menuID.java
@@ -2,6 +2,7 @@ package com.example.monstersawiki;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,12 @@ import android.widget.RadioButton;
 
 public class menuID extends AppCompatActivity {
 
+    public final static String EXTRA_MESSAGE = ".MESSAGE";
+
+    EditText edit1;
+    RadioButton s, b, m;
+    Button create;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,11 +24,40 @@ public class menuID extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
-        EditText edit1;
-        RadioButton s,b,m;
-        Button create;
+        edit1 = findViewById(R.id.txtName);
+        s = findViewById(R.id.sulley);
+        b = findViewById(R.id.boo);
+        m = findViewById(R.id.mike);
+        create = findViewById(R.id.btnCreate);
+
     }
-    public void createID(View view){
-        
+
+    public void createID(View view) {
+        if (s.isChecked() == true && b.isChecked() == false && m.isChecked() == false) {
+            Intent intent = new Intent(this, sulley.class);
+
+            String message = edit1.getText().toString();
+
+            intent.putExtra(EXTRA_MESSAGE, message);
+
+            startActivity(intent);
+        } else if (s.isChecked() == false && b.isChecked() == true && m.isChecked() == false) {
+            Intent intent = new Intent(this, boo.class);
+
+            String message = edit1.getText().toString();
+
+            intent.putExtra(EXTRA_MESSAGE, message);
+
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(this, mike.class);
+
+            String message = edit1.getText().toString();
+
+            intent.putExtra(EXTRA_MESSAGE, message);
+
+            startActivity(intent);
+
+        }
     }
 }
--- app/src/main/java/com/example/monstersawiki/sulley.java
+++ app/src/main/java/com/example/monstersawiki/sulley.java
@@ -2,13 +2,27 @@ package com.example.monstersawiki;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class sulley extends AppCompatActivity {
+
+    TextView NameBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sulley);
+
+        getSupportActionBar().hide();
+
+        NameBack = findViewById(R.id.txtNameBack);
+
+        Intent intentDisplay = getIntent();
+        String messageDisplay = intentDisplay.getStringExtra(menuID.EXTRA_MESSAGE);
+
+        NameBack.setText(messageDisplay);
+
     }
 }
