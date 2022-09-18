cdd46a440177f8da532893b569945d87b653b56c
Maria Cavalcante <maria.nascimento343@etec.sp.gov.br>
2022-09-18 20:10:59 -0300

    update quiz

--- app/src/main/java/com/example/monstersawiki/quiz.java
+++ app/src/main/java/com/example/monstersawiki/quiz.java
@@ -11,7 +11,7 @@ import android.widget.RadioButton;
 
 public class quiz extends AppCompatActivity {
 
-    RadioButton yes1, yes2, no1, no2;
+    RadioButton yes1, yes2, yes3, no1, no2, no3;
     Button find;
 
     @Override
@@ -23,24 +23,46 @@ public class quiz extends AppCompatActivity {
 
         yes1 = findViewById(R.id.btnYes1);
         yes2 = findViewById(R.id.btnYes2);
+        yes3 = findViewById(R.id.btnYes3);
         no1 = findViewById(R.id.btnNo1);
         no2 = findViewById(R.id.btnNo2);
+        no3 = findViewById(R.id.btnNo3);
         find = findViewById(R.id.btnFind);
     }
 
     public void answerQuiz(View view){
-        if(yes1.isChecked() == true && no1.isChecked() == false && yes2.isChecked() == true && no2.isChecked() == false){
+        if(yes1.isChecked() == true && yes2.isChecked() == true && yes3.isChecked() == true){
             Uri uri = Uri.parse("https://pixar.fandom.com/pt/wiki/Henry_J._Waternoose");
             Intent it = new Intent(Intent.ACTION_VIEW,uri);
             startActivity(Intent.createChooser(it, getString(R.string.chNavegador)));
         }
-        else if(yes1.isChecked() == false && no1.isChecked() == true && yes2.isChecked() == false && no2.isChecked() == true){
+        else if(no1.isChecked() == true && no2.isChecked() == true && no3.isChecked() == true){
             Uri uri = Uri.parse("https://disney.fandom.com/wiki/Randall_Boggs");
             Intent it = new Intent(Intent.ACTION_VIEW,uri);
             startActivity(Intent.createChooser(it, getString(R.string.chNavegador)));
         }
-        else if(yes1.isChecked() == true && no1.isChecked() == false && yes2.isChecked() == false && no2.isChecked() == true){
+        else if(yes1.isChecked() == true && no2.isChecked() == true && no3.isChecked() == true){
             Uri uri = Uri.parse("https://pixar.fandom.com/wiki/James_P._Sullivan");
+            Intent it = new Intent(Intent.ACTION_VIEW,uri);
+            startActivity(Intent.createChooser(it, getString(R.string.chNavegador)));
+        }
+        else if(yes1.isChecked() == true && yes2.isChecked() == true && no3.isChecked() == true){
+            Uri uri = Uri.parse("https://disney.fandom.com/pt-br/wiki/Roz");
+            Intent it = new Intent(Intent.ACTION_VIEW,uri);
+            startActivity(Intent.createChooser(it, getString(R.string.chNavegador)));
+        }
+        else if(no1.isChecked() == true && yes2.isChecked() == true && yes3.isChecked() == true){
+            Uri uri = Uri.parse("https://pixar.fandom.com/pt/wiki/Yeti");
+            Intent it = new Intent(Intent.ACTION_VIEW,uri);
+            startActivity(Intent.createChooser(it, getString(R.string.chNavegador)));
+        }
+        else if(no1.isChecked() == true && no2.isChecked() == true && yes3.isChecked() == true){
+            Uri uri = Uri.parse("https://disney.fandom.com/wiki/George_Sanderson");
+            Intent it = new Intent(Intent.ACTION_VIEW,uri);
+            startActivity(Intent.createChooser(it, getString(R.string.chNavegador)));
+        }
+        else if(yes1.isChecked() == true && no2.isChecked() == true && yes3.isChecked() == true){
+            Uri uri = Uri.parse("https://disney.fandom.com/wiki/Thaddeus_Bile");
             Intent it = new Intent(Intent.ACTION_VIEW,uri);
             startActivity(Intent.createChooser(it, getString(R.string.chNavegador)));
         }
