8f503f1d90f0f601fe0aa52982d8170e280c9ef5
Maria Cavalcante <maria.nascimento343@etec.sp.gov.br>
2022-09-18 00:46:06 -0300

    botão quiz e finalização tela história

--- app/src/main/java/com/example/monstersawiki/story.java
+++ app/src/main/java/com/example/monstersawiki/story.java
@@ -2,9 +2,13 @@ package com.example.monstersawiki;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class story extends AppCompatActivity {
 
@@ -14,6 +18,14 @@ public class story extends AppCompatActivity {
         setContentView(R.layout.activity_story);
 
         getSupportActionBar().hide();
+
+    }
+    public void openAbout(View view){
+        Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+        String query = "Monstros S.A.";
+        intent.putExtra(SearchManager.QUERY, query);
+        startActivity(intent);
+
     }
 
     public void openID(View view){
