3b8fce8a9fc8029e9896844ea5c881598086514e
NMV <98959079+NicoNMV@users.noreply.github.com>
2022-09-20 21:08:30 -0300

    loc

---
+++ main/java/com/example/monstersawiki/door.java
@@ -0,0 +1,40 @@
+package com.example.monstersawiki;
+
+import static com.example.monstersawiki.R.id.btnporta_local;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class door extends AppCompatActivity {
+
+    ImageButton btnporta;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_door);
+        btnporta = findViewById(btnporta_local);
+    }
+
+    public void mostrarLoc(View view) {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(door.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            return;
+        }
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        LocationListener locationListener = new ClassLocation();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+    }
+
+}
---
+++ main/java/com/example/monstersawiki/splash.java
@@ -0,0 +1,44 @@
+package com.example.monstersawiki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+
+public class splash extends AppCompatActivity {
+
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        /*new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                startActivity(new Intent(getBaseContext(), MainActivity.class));
+                finish();
+           }
+       },5000); */
+
+        progressBar = findViewById(R.id.progress_bar);
+
+        progressBar.setMax(100);
+        progressBar.setScaleY(3f);
+
+        progressAnimation();
+    }
+
+    public void progressAnimation(){
+        ProgressBarAnimation anim = new ProgressBarAnimation(this, progressBar, 0f, 100f);
+        anim.setDuration(7000);
+        progressBar.setAnimation(anim);
+    }
+}
