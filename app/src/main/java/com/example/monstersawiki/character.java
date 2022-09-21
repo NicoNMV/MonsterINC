524ac89b56ed2d8fd1203eba295821cf5dd98d93
Maria Cavalcante <101808675+MariaEduCavalcante@users.noreply.github.com>
2022-09-21 14:38:21 -0300

    botões

--- app/src/main/java/com/example/monstersawiki/character.java
+++ app/src/main/java/com/example/monstersawiki/character.java
@@ -19,4 +19,47 @@ public class character extends AppCompatActivity {
         Intent intent = new Intent(this, menuID.class);
         startActivity(intent);
     }
+    
+    public void openCena1(View view){
+        Uri uri = Uri.parse("https://youtu.be/wV_FMbJ2e6w");
+        Intent it = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(Intent.createChooser(it, getString(R.string.chNavegador)));
+    }
+    public void openCena2(View view){
+        Uri uri = Uri.parse("https://youtu.be/uBFhZThWq98");
+        Intent it = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(Intent.createChooser(it, getString(R.string.chNavegador)));
+    }
+    public void openCena3(View view){
+        Uri uri = Uri.parse("https://youtu.be/qe72QwfbzTo");
+        Intent it = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(Intent.createChooser(it, getString(R.string.chNavegador)));
+    }
+
+    public void openID(View view){
+        Intent intent = new Intent(this, menuID.class);
+        startActivity(intent);
+    }
+
+    public void openCharacter(View view){
+        Intent intent = new Intent(this, character.class);
+        startActivity(intent);
+    }
+    public void openStory(View view){
+        Intent intent = new Intent(this, story.class);
+        startActivity(intent);
+    }
+    public void openDoor(View view){
+        Intent intent = new Intent(this, door.class);
+        startActivity(intent);
+    }
+    public void openUniversity(View view){
+        Intent intent = new Intent(this, university.class);
+        startActivity(intent);
+    }
+    public void openHome(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
