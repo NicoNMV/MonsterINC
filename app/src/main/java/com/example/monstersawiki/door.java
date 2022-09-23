938c9d8a0f5e40ce6d770ad10026452132d33599
NMV <nicolas.viana2@etec.sp.gov.br>
2022-09-23 09:21:26 -0300

    door

--- app/src/main/java/com/example/monstersawiki/door.java
+++ app/src/main/java/com/example/monstersawiki/door.java
@@ -4,6 +4,7 @@ import static com.example.monstersawiki.ClassLocation.latitude;
 import static com.example.monstersawiki.ClassLocation.longitude;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -11,97 +12,115 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 public class door extends AppCompatActivity implements  SensorEventListener {
 
+    RadioButton rd1,rd2,rd3;
+    double[] himalaias = {34.1504, 78.4687},
+            tokyo = {35.7117, 139.3967},
+            hawaii = {19.5711, -155.5355};
+
     SensorManager sensorManager;
     Sensor sensor;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_door);
 
+        getSupportActionBar().hide();
+
+        rd1 = findViewById(R.id.rdbDoor1);
+        rd2 = findViewById(R.id.rdbDoor2);
+        rd3 = findViewById(R.id.rdbDoor3);
+
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         sensorManager.registerListener( this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
-        }
+    }
 
-        @Override
-        public void onSensorChanged(SensorEvent event) {
+    @Override
+    public void onSensorChanged(SensorEvent event) {
         float x = event.values[0],
                 y = event.values[1],
                 z = event.values[2];
-        }
+    }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
 
-    public void buscarGPS(View v) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(door.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-                ActivityCompat.requestPermissions(door.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
-                ActivityCompat.requestPermissions(door.this, new String[]{Manifest.permission.ACCESS_NETWORK_STATE}, 1);
-                return;
-            }
-            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-            LocationListener locationListener = new ClassLocation();
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
-            if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-                Uri location = Uri.parse(("https://www.google.com/maps/search/?api=1&query=" + latitude + "," + longitude));
-            }
-            else{}
+    public void locDoor(View view){
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(door.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            ActivityCompat.requestPermissions(door.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            ActivityCompat.requestPermissions(door.this, new String[]{Manifest.permission.ACCESS_NETWORK_STATE}, 1);
+            return;
         }
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        LocationListener locationListener = new ClassLocation();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
-        @Override
-        public void onLocationChanged(@NonNull Location location) {
-        latitude = location.getLatitude();
-        longitude = location.getLongitude();
+        if(rd1.isChecked() == true){
+            Uri uri = Uri.parse("https://goo.gl/maps/FfGhsWWbtM9SJiAM9");
+            Intent it = new Intent(Intent.ACTION_VIEW,uri);
+            startActivity(Intent.createChooser(it, getString(R.string.chNavegador)));
         }
-
-
-
-        public void openID(View view) {
-            Intent intent = new Intent(this, menuID.class);
-            startActivity(intent);
+        else if(rd2.isChecked() == true){
+            Uri uri = Uri.parse("https://goo.gl/maps/G9JxDatLG8TtUXds8");
+            Intent it = new Intent(Intent.ACTION_VIEW,uri);
+            startActivity(Intent.createChooser(it, getString(R.string.chNavegador)));
         }
-
-        public void openCharacter(View view) {
-            Intent intent = new Intent(this, character.class);
-            startActivity(intent);
+        else{
+            Uri uri = Uri.parse("https://goo.gl/maps/8YTGewccjgA8SGFV8");
+            Intent it = new Intent(Intent.ACTION_VIEW,uri);
+            startActivity(Intent.createChooser(it, getString(R.string.chNavegador)));
         }
-
-        public void openStory(View view) {
-            Intent intent = new Intent(this, story.class);
-            startActivity(intent);
-        }
-
-        public void openDoor(View view) {
-            Intent intent = new Intent(this, door.class);
-            startActivity(intent);
-        }
-
-        public void openUniversity(View view) {
-            Intent intent = new Intent(this, university.class);
-            startActivity(intent);
-        }
-
-        public void openHome(View view) {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-        }
-
     }
+
+    public void openID(View view) {
+        Intent intent = new Intent(this, menuID.class);
+        startActivity(intent);
+    }
+
+    public void openCharacter(View view) {
+        Intent intent = new Intent(this, character.class);
+        startActivity(intent);
+    }
+
+    public void openStory(View view) {
+        Intent intent = new Intent(this, story.class);
+        startActivity(intent);
+    }
+
+    public void openDoor(View view) {
+        Intent intent = new Intent(this, door.class);
+        startActivity(intent);
+    }
+
+    public void openUniversity(View view) {
+        Intent intent = new Intent(this, university.class);
+        startActivity(intent);
+    }
+
+    public void openHome(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}
